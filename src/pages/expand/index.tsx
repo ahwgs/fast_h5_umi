9eeae60eca20e1a46f6f26d34f8b239ee752f5d8
ahwgs <ah_wgs@126.com>
2020-06-14 01:49:33 +0800

    docs: 编写相关文档

--- src/pages/expand/index.tsx
+++ src/pages/expand/index.tsx
@@ -1,22 +1,38 @@
 import React from 'react';
-import { ConnectState } from '@/models/connect';
-import { connect, ConnectProps } from 'umi';
-import './index.less';
+import { Accordion, List } from 'antd-mobile';
+import { history } from 'umi';
+import styles from './index.less';
 
-interface IProps extends ConnectProps {
-  loading: boolean;
-}
+const components = [
+  {
+    title: 'Basic 基础',
+    key: 'basic',
+    list: [
+      {
+        title: '权限组件',
+        path: '/expand/auth',
+      },
+    ],
+  },
+];
 
-const ExpandPage: React.FC<IProps> = props => {
-  const { loading } = props;
-  console.log('loading', loading);
-  return <div>拓展页</div>;
-};
+const ExpandPage: React.FC = () => (
+  <section className={styles.container}>
+    <header>拓展组件</header>
+    <Accordion>
+      {components.map(group => (
+        <Accordion.Panel key={group.key} header={group.title}>
+          <List key={group.key}>
+            {group.list.map(com => (
+              <List.Item onClick={() => history.push(com.path)} key={com.path}>
+                {com.title}
+              </List.Item>
+            ))}
+          </List>
+        </Accordion.Panel>
+      ))}
+    </Accordion>
+  </section>
+);
 
-ExpandPage.defaultProps = {
-  loading: false,
-};
-
-export default connect(({ loading }: ConnectState) => ({
-  loading: loading.effects[''],
-}))(ExpandPage);
+export default ExpandPage;
